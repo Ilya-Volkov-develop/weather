662e936501632b52b234ad6f58cb19c516bf8e4f
ilia <ilia.volkov2016@mail.ru>
2022-09-28 16:18:52 +0300

    Создание карточки погоды и прочие настройки

---
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.weather.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weather.R
+import com.example.weather.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
